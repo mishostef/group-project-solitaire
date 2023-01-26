5396660cd156bbe20e29512c0fdc8cb7031dfa5b
Antoniya Lambova <antoniya.lambova@amusnet.com>
2023-01-27 00:53:17 +0200

    add all cards

--- solitaire-client/src/app.ts
+++ solitaire-client/src/app.ts
@@ -1,62 +1,68 @@
-import { Connection } from "./Connection";
-import { engine } from "./engine";
-import * as PIXI from "pixi.js";
-
-import { gsap } from "gsap";
-import { PixiPlugin } from "gsap/PixiPlugin.js";
-import { createBox, transformTo, turnCard } from "./utils";
+import * as PIXI from 'pixi.js';
+import { gsap } from 'gsap';
+import { PixiPlugin } from 'gsap/PixiPlugin';
 
 gsap.registerPlugin(PixiPlugin);
 PixiPlugin.registerPIXI(PIXI);
 
-const initForm = document.querySelector("form");
-const initSection = document.getElementById("init");
-const gameSection = document.getElementById("game");
 
-let connection = null;
-
-initForm.addEventListener("submit", async (event) => {
-  event.preventDefault();
-  const { nickname } = Object.fromEntries(
-    new FormData(event.target as HTMLFormElement)
-  );
-
-  connection = new Connection(nickname as string);
-  await connection.open();
-  engine(connection);
-  showBoard();
-
-  connection.send("startGame");
+const app = new PIXI.Application({
+    width: 1800, 
+    height: 1800,
+    background: 0x999999
 });
 
-document.getElementById("disconnect").addEventListener("click", () => {
-  connection?.disconnect();
-  showInit();
-});
 
-function showBoard() {
-  initSection.style.display = "none";
-  gameSection.style.display = "block";
+document.body.appendChild(app.view as HTMLCanvasElement);
 
-  test();
+
+const spritesheet = PIXI.BaseTexture.from('assets/22331.jpg');
+
+
+const clubs = [];
+sliceDeck(clubs, 47, 847, 50);
+console.log(clubs);
+
+const hearts = [];
+sliceDeck(hearts, 47, 1507, 200);
+console.log(hearts);
+
+const spades = [];
+sliceDeck(spades, 47, 2167, 350);
+console.log(spades);
+
+const diamonds = [];
+sliceDeck(diamonds, 47, 2827, 500);
+console.log(diamonds);
+
+function sliceCard(x: number, y: number, w:  number, h: number) {
+    
+    const cardTexture = new PIXI.Texture(spritesheet, new PIXI.Rectangle(x, y, w, h));
+    const card = new PIXI.Sprite(cardTexture);
+
+    gsap.set(card, {pixi: {scale: 0.2}})
+    
+    app.stage.addChild(card);
+
+  
+    return card;
 }
 
-export function test() {
-  const board = document.getElementById("board");
-  const app = new PIXI.Application({
-    width: 800,
-    height: 600,
-  });
-  board.appendChild(app.view as HTMLCanvasElement);
-  const front = createBox(300, 300, 0xa777aa, 50, 100);
-  const back = createBox(300, 300, 0xe777e, 50, 100);
 
-  app.stage.addChild(front);
-  app.stage.addChild(back);
-  turnCard(back, front);
-}
+function sliceDeck(arr, x: number, y: number, row) {
+    const width = 410;
+    const height = 623;
+    for ( let i = 0; i < 13; i++) {
+        
+        const startX = x;
+        const startY = y;
+    
+        const card = sliceCard(startX + (i*width) + (i * 48) + i/2, startY, width, height);
+        card.position.set(20 + (i*100), row);
+        arr.push(card)
+    
+    }
 
-function showInit() {
-  initSection.style.display = "block";
-  gameSection.style.display = "none";
+    return arr;
+
 }
