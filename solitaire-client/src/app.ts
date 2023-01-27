1aba748d678c99d59800ab29ed9826516ba6144c
mihail.stefanov <mihail.stefanov@amusnet.com>
2023-01-27 17:14:29 +0200

    added class Card, clearscreen

--- solitaire-client/src/app.ts
+++ solitaire-client/src/app.ts
@@ -7,7 +7,7 @@ import { PixiPlugin } from "gsap/PixiPlugin.js";
 import { clearScreen, createBox, getMask, turnCard } from "./utils";
 import { Card } from "./Card";
 import { Suits } from "./constans";
-
+import { test } from "./utils";
 gsap.registerPlugin(PixiPlugin);
 PixiPlugin.registerPIXI(PIXI);
 
@@ -37,81 +37,53 @@ sliceDeck(spades, 47, 2167, 350);
 const diamonds = [];
 sliceDeck(diamonds, 47, 2827, 500);
 
-
 //const card = new Card(400, 400, "A", Suits.hearts);
 
-export function test() {
-  // const board = document.getElementById("board");
-  // const app = new PIXI.Application({
-  //   width: 800,
-  //   height: 600,
-  // });
-  // board.appendChild(app.view as HTMLCanvasElement);
-  clearScreen(app);
-  const front = createBox(300, 300, 0xa777aa, 50, 100);
-  const back = createBox(300, 300, 0xe777e, 50, 100);
-  const masked = createBox(300, 500, 0xaa55555, 100, 100);
+function sliceCard(x: number, y: number, w: number, h: number) {
+  const cardTexture = new PIXI.Texture(
+    spritesheet,
+    new PIXI.Rectangle(x, y, w, h)
+  );
+  const card = new PIXI.Sprite(cardTexture);
+  card.scale.set(scale);
 
-  const mask = getMask(300, 500, 5);
-  app.stage.addChild(mask);
+  app.stage.addChild(card);
 
-  masked.mask = mask;
-  app.stage.addChild(masked);
-
-  app.stage.addChild(front);
-  app.stage.addChild(back);
-  turnCard(back, front);
-}
-
-function sliceCard(x: number, y: number, w:  number, h: number) {
-    
-    const cardTexture = new PIXI.Texture(spritesheet, new PIXI.Rectangle(x, y, w, h));
-    const card = new PIXI.Sprite(cardTexture);
-    card.scale.set(scale);
-    
-    app.stage.addChild(card);
-     
-    return card;
+  return card;
 }
 
 function sliceDeck(arr, x: number, y: number, row) {
-    for ( let i = 0; i < 13; i++) {
-        
-        const card = sliceCard(x + (i*width) + (i * 48) + i/2, y, width, height);
-        
-        card.position.set(20 + (i*100), row);
-        arr.push(card)
+  for (let i = 0; i < 13; i++) {
+    const card = sliceCard(x + i * width + i * 48 + i / 2, y, width, height);
 
-        const mask = new PIXI.Graphics();
-        mask.beginFill(0);
-        mask. drawRoundedRect(5, 5, width-8, height-5, 45);
-        mask.endFill();
-        mask.scale.set(scale);
-        card.mask = mask;
+    card.position.set(20 + i * 100, row);
+    arr.push(card);
 
-        app.stage.addChild(mask);
-            
-        mask.position.set(card.x, card.y);
-    
-    }
+    const mask = new PIXI.Graphics();
+    mask.beginFill(0);
+    mask.drawRoundedRect(5, 5, width - 8, height - 5, 45);
+    mask.endFill();
+    mask.scale.set(scale);
+    card.mask = mask;
 
-    return arr;
+    app.stage.addChild(mask);
 
+    mask.position.set(card.x, card.y);
+  }
+
+  return arr;
 }
 
 //backCard()
 function backCard() {
+  const cardTexture = new PIXI.Texture(spritesheet);
+  const card = new PIXI.Sprite(cardTexture);
+  card.scale.set(scale);
 
-    const cardTexture = new PIXI.Texture(spritesheet);
-    const card = new PIXI.Sprite(cardTexture);
-    card.scale.set(scale);
-    
-    app.stage.addChild(card);
-     
-    return card;
+  app.stage.addChild(card);
 
+  return card;
 }
-
 
 const initForm = document.querySelector("form");
 const initSection = document.getElementById("init");
