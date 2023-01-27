477281c1febf13890fa0f741beed427b3aba25b1
mihail.stefanov <mihail.stefanov@amusnet.com>
2023-01-27 16:40:26 +0200

    added class Card, clearscreen

---
+++ solitaire-client/src/Card.ts
@@ -0,0 +1,19 @@
+import * as PIXI from "pixi.js";
+import { Container, DisplayObject } from "pixi.js";
+import { CARD_HEIGHT, CARD_WIDTH, Face, Suits } from "./constans";
+import { createBox, turnCard } from "./utils";
+import { app } from "./app";
+
+export class Card extends Container {
+  back: DisplayObject;
+  front: DisplayObject;
+  constructor(x, y, public face: Face, public suit: Suits) {
+    super();
+    this.face = face;
+    this.suit = suit;
+    this.front = createBox(x, y, 0x0000ff, CARD_WIDTH / 2, CARD_HEIGHT / 2); ///will be changed
+    this.back = createBox(x, y, 0xff0000, CARD_WIDTH / 2, CARD_HEIGHT / 2); ///will be changed
+    app.stage.addChild(this.front);
+    app.stage.addChild(this.back);
+  }
+}
--- solitaire-client/src/app.ts
+++ solitaire-client/src/app.ts
@@ -4,26 +4,22 @@ import * as PIXI from "pixi.js";
 
 import { gsap } from "gsap";
 import { PixiPlugin } from "gsap/PixiPlugin.js";
-import { createBox, getMask, turnCard } from "./utils";
-
-
+import { clearScreen, createBox, getMask, turnCard } from "./utils";
+import { Card } from "./Card";
+import { Suits } from "./constans";
 
 gsap.registerPlugin(PixiPlugin);
 PixiPlugin.registerPIXI(PIXI);
 
-
-const app = new PIXI.Application({
-    width: 1800, 
-    height: 1800,
-    background: 0x999999
+export const app = new PIXI.Application({
+  width: 1800,
+  height: 1800,
+  background: 0x999999,
 });
-
 
 document.body.appendChild(app.view as HTMLCanvasElement);
 
-
-const spritesheet = PIXI.BaseTexture.from('assets/22331.jpg');
-
+const spritesheet = PIXI.BaseTexture.from("assets/22331.jpg");
 
 const clubs = [];
 sliceDeck(clubs, 47, 847, 50);
@@ -37,62 +33,60 @@ sliceDeck(spades, 47, 2167, 350);
 const diamonds = [];
 sliceDeck(diamonds, 47, 2827, 500);
 
+console.log("hearts", hearts);
+// const card = new Card(400, 400, "A", Suits.hearts);
 
 export function test() {
-  const board = document.getElementById("board");
-  const app = new PIXI.Application({
-    width: 800,
-    height: 600,
-  });
-  board.appendChild(app.view as HTMLCanvasElement);
+  clearScreen(app);
   const front = createBox(300, 300, 0xa777aa, 50, 100);
   const back = createBox(300, 300, 0xe777e, 50, 100);
   const masked = createBox(300, 500, 0xaa55555, 100, 100);
-  
-  const mask = getMask(300,500, 5);
+
+  const mask = getMask(300, 500, 5);
   app.stage.addChild(mask);
 
   masked.mask = mask;
   app.stage.addChild(masked);
-  
+
   app.stage.addChild(front);
   app.stage.addChild(back);
+  const card = new Card(400, 400, "A", Suits.hearts);
   turnCard(back, front);
 }
 
+function sliceCard(x: number, y: number, w: number, h: number) {
+  const cardTexture = new PIXI.Texture(
+    spritesheet,
+    new PIXI.Rectangle(x, y, w, h)
+  );
+  const card = new PIXI.Sprite(cardTexture);
 
-function sliceCard(x: number, y: number, w:  number, h: number) {
-    
-    const cardTexture = new PIXI.Texture(spritesheet, new PIXI.Rectangle(x, y, w, h));
-    const card = new PIXI.Sprite(cardTexture);
+  gsap.set(card, { pixi: { scale: 0.2 } });
 
-    gsap.set(card, {pixi: {scale: 0.2}})
-    
-    app.stage.addChild(card);
+  app.stage.addChild(card);
 
-  
-    return card;
+  return card;
 }
-
 
 function sliceDeck(arr, x: number, y: number, row) {
-    const width = 410;
-    const height = 623;
-    for ( let i = 0; i < 13; i++) {
-        
-        const startX = x;
-        const startY = y;
-    
-        const card = sliceCard(startX + (i*width) + (i * 48) + i/2, startY, width, height);
-        card.position.set(20 + (i*100), row);
-        arr.push(card)
-    
-    }
+  const width = 410;
+  const height = 623;
+  for (let i = 0; i < 13; i++) {
+    const startX = x;
+    const startY = y;
 
-    return arr;
+    const card = sliceCard(
+      startX + i * width + i * 48 + i / 2,
+      startY,
+      width,
+      height
+    );
+    card.position.set(20 + i * 100, row);
+    arr.push(card);
+  }
 
+  return arr;
 }
-
 
 const initForm = document.querySelector("form");
 const initSection = document.getElementById("init");
@@ -130,5 +124,3 @@ function showInit() {
   initSection.style.display = "block";
   gameSection.style.display = "none";
 }
-
-
