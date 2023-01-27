1eda7aa7564f50da3f612761e24f67a6882091dd
mihail.stefanov <mihail.stefanov@amusnet.com>
2023-01-27 22:39:03 +0200

    set  anchor in sliceCard, backCard, sliceDeck

--- solitaire-client/src/cardsTexture.ts
+++ solitaire-client/src/cardsTexture.ts
@@ -11,57 +11,65 @@ PixiPlugin.registerPIXI(PIXI);
 
 const spritesheet = PIXI.BaseTexture.from("assets/22331.jpg");
 
-function sliceCard(x: number, y: number, w:  number, h: number) {
-    
-    const cardTexture = new PIXI.Texture(spritesheet, new PIXI.Rectangle(x, y, w, h));
-    const card = new PIXI.Sprite(cardTexture);
-    card.scale.set(CARD_SCALE);
-    
-    app.stage.addChild(card);
-     
-    return card;
+function sliceCard(x: number, y: number, w: number, h: number) {
+  const cardTexture = new PIXI.Texture(
+    spritesheet,
+    new PIXI.Rectangle(x, y, w, h)
+  );
+  const card = new PIXI.Sprite(cardTexture);
+  card.anchor.set(0.5);
+  card.scale.set(CARD_SCALE);
+  app.stage.addChild(card);
+
+  return card;
 }
 
 export function sliceDeck(arr, x: number, y: number, row) {
-    for ( let i = 0; i < 13; i++) {
-        
-        const card = sliceCard(x + (i*CARD_WIDTH) + (i * 48) + i/2, y, CARD_WIDTH, CARD_HEIGHT);
-        const back = backCard();   
+  for (let i = 0; i < 13; i++) {
+    const card = sliceCard(
+      x + i * CARD_WIDTH + i * 48 + i / 2,
+      y,
+      CARD_WIDTH,
+      CARD_HEIGHT
+    );
+    const back = backCard();
+    card.position.set(20 + i * 100, row);
 
-        card.position.set(20 + (i*100), row);
-        back.position.set(20 + (i*100), row);
+    back.position.set(20 + i * 100, row);
 
-        arr.push(card)
+    arr.push(card);
 
-        const mask = new PIXI.Graphics();
-        mask.beginFill(0);
-        mask. drawRoundedRect(5, 5, CARD_WIDTH-8, CARD_HEIGHT-5, 45);
-        mask.endFill();
-        mask.scale.set(CARD_SCALE);
-        card.mask = mask;
+    const mask = new PIXI.Graphics();
+    mask.beginFill(0);
+    // mask.drawRoundedRect(5, 5, CARD_WIDTH - 8, CARD_HEIGHT - 5, 45);
+    mask.drawRoundedRect(
+      5 - (CARD_WIDTH - 8) / 2,
+      5 - (CARD_HEIGHT - 5) / 2,
+      CARD_WIDTH - 8,
+      CARD_HEIGHT - 5,
+      45
+    );
+    mask.endFill();
+    mask.scale.set(CARD_SCALE);
 
-        app.stage.addChild(mask);
-            
-        mask.position.set(card.x, card.y);
+    card.mask = mask;
 
-        turnCard(card, back);
-    
-    }
+    app.stage.addChild(mask);
 
-    return arr;
+    mask.position.set(card.x, card.y);
 
+    turnCard(card, back);
+  }
+
+  return arr;
 }
 
 //backCard()
 export function backCard() {
-
-
-    const cardTexture = PIXI.Texture.from("assets/back.png");
-    const card = new PIXI.Sprite(cardTexture);
-    card.scale.set(CARD_SCALE);
-    
-    app.stage.addChild(card);
-     
-    return card;
-
+  const cardTexture = PIXI.Texture.from("assets/back.png");
+  const card = new PIXI.Sprite(cardTexture);
+  card.scale.set(CARD_SCALE);
+  card.anchor.set(0.5); //
+  app.stage.addChild(card);
+  return card;
 }
--- solitaire-client/src/utils.ts
+++ solitaire-client/src/utils.ts
@@ -11,15 +11,16 @@ gsap.registerPlugin(PixiPlugin);
 PixiPlugin.registerPIXI(PIXI);
 
 export function turnCard(back: DisplayObject, front: DisplayObject) {
+  const duration = 0.5;
   const tl = gsap.timeline();
   back.alpha = 0;
   gsap.set(back, { pixi: { skewY: 90 } });
   front.interactive = true;
   front.on("pointertap", () => {
-    tl.to(front, { pixi: { skewY: -90 }, duration: 0.05 });
+    tl.to(front, { pixi: { skewY: -90 }, duration });
     tl.to(back, {
       pixi: { skewY: 0, alpha: 1 },
-      duration: 0.05,
+      duration,
     });
   });
 }
