fa1dde9f471277597607ab4bb39ceaf20bc4bf01
mihail.stefanov <mihail.stefanov@amusnet.com>
2023-01-28 19:41:46 +0200

    added methods getCardBack and placeCard to Card class

--- solitaire-client/src/Card.ts
+++ solitaire-client/src/Card.ts
@@ -1,21 +1,51 @@
 import * as PIXI from "pixi.js";
 import { Container, DisplayObject } from "pixi.js";
-import { CARD_HEIGHT, CARD_WIDTH, Face, Suits } from "./constans";
+import { CARD_HEIGHT, CARD_SCALE, CARD_WIDTH, Face, Suits } from "./constans";
 import { createBox, turnCard } from "./utils";
-import { app } from "./app";
+//import { app } from "./app";
+import { createDeckAssets } from "./app";
 
 export class Card extends Container {
-  back: DisplayObject;
-  front: DisplayObject;
-  constructor(x, y, public face: Face, public suit: Suits) {
+  public back: DisplayObject;
+  public front: DisplayObject;
+  private app: PIXI.Application;
+  constructor(public face: Face, public suit: Suits, app) {
     super();
     this.face = face;
     this.suit = suit;
-    this.front = createBox(x, y, 0x0000ff, CARD_WIDTH / 2, CARD_HEIGHT / 2); ///will be changed
-    this.front.scale.set(0.5);
-    this.back = createBox(x, y, 0xff0000, CARD_WIDTH / 2, CARD_HEIGHT / 2); ///will be changed
-    this.front.scale.set(0.5);
-    app.stage.addChild(this.front);
-    app.stage.addChild(this.back);
+    this.app = app;
+    this.front = createDeckAssets()[`${face}${Suits[suit]}`];
+    this.back = this.getCardBack();
+  }
+
+  getCardBack() {
+    const backTexture = PIXI.Texture.from("assets/back.png");
+    const back = new PIXI.Sprite(backTexture);
+    back.scale.set(CARD_SCALE);
+    back.anchor.set(0.5);
+    return back;
+  }
+
+  placeCard(x: number, y: number) {
+    this.app.stage.addChild(this.front);
+    this.app.stage.addChild(this.back);
+    this.front.position.set(x, y);
+    this.back.position.set(x, y);
+    const mask = new PIXI.Graphics();
+    mask.beginFill(0);
+    mask.drawRoundedRect(5, 5, CARD_WIDTH - 8, CARD_HEIGHT - 5, 45);
+    mask.drawRoundedRect(
+      5 - (CARD_WIDTH - 8) / 2,
+      5 - (CARD_HEIGHT - 5) / 2,
+      CARD_WIDTH - 8,
+      CARD_HEIGHT - 5,
+      45
+    );
+    mask.endFill();
+    mask.scale.set(CARD_SCALE);
+    this.front.mask = mask;
+    this.app.stage.addChild(mask);
+    mask.position.set(x, y);
+    turnCard(this.front, this.back);
   }
 }
